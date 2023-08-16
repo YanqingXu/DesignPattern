c74399e7af181d48dc4dcaf72eaff7bceb002fb4
YanqingXu <xyq949587362xyq007@163.com>
2023-08-16 23:55:37 +0800

    修正lua的单例模式

--- Creation/Singleton/Lua/singleton.lua
+++ Creation/Singleton/Lua/singleton.lua
@@ -12,4 +12,20 @@ function Singleton:new(o, ...)
     return instance
 end
 
-return Singleton
+local MyClass = Singleton:new()
+
+function MyClass:display()
+    print("MyClass instance!")
+end
+
+local instance1 = MyClass:new()
+local instance2 = MyClass:new()
+
+instance1:display()
+instance2:display()
+
+if instance1 == instance2 then
+    print("两个实例是相同的")
+else
+    print("两个实例是不同的")
+end
